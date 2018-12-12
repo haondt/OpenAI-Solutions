5f4c6b53e13d52aed27ced930bf4a198ddc3b962
Noah Burghardt <burghardtnoah@gmail.com>
2018-12-12 11:06:56 -0700

    finished frozenlake agent

--- FrozenLake8x8-v0/Agent.py
+++ FrozenLake8x8-v0/Agent.py
@@ -1,8 +1,10 @@
 # Agent
 # Based off the agent define in the RLGlue Software
 # https://sites.google.com/a/rl-community.org/rl-glue/Home?authuser=0
-
 # Modified by Noah Burghardt
+
+# Implementation of the Tabular Dyna-Q agent defined in Chapter 8 
+# of Sutton and Barto
 import numpy as np
 
 class Agent:
@@ -11,6 +13,25 @@ class Agent:
 
 	ie. These methods must be defined in your own Agent classes
 	"""
+
+	planningSteps = 1
+	lastState = None
+	lastAction = None
+	Q = None
+	model = None
+	epsilon = 0.1
+	stepSize = 0.5
+	observedStates = None
+	
+	# actions are defined as
+	# 0: left
+	# 1: down
+	# 2: right
+	# 3: up
+	actions = list(range(4))
+
+	# states are defined as 64 discrete numbers
+	states = np.linspace(0,63,64)
 	
 	def __init__(self, width, height, moveset='normal'):
 		"""Declare agent variables."""
@@ -21,7 +42,9 @@ class Agent:
 		run once, in experiment
 		Initialize agent variables.
 		"""
-		pass
+		self.Q = {(state, action): 0 for state in self.states for action in self.actions}
+		self.model = {}
+		observedStates= {}
 
 	def agent_start(self, state):
 		"""
@@ -34,7 +57,9 @@ class Agent:
 		Returns:
 			The first action the agent takes.
 		"""
-		return None
+		self.lastState = state
+		self.lastAction = self.epGreedy(state)
+		return self.lastAction
 
 	def agent_step(self, reward, state):
 		"""
@@ -45,7 +70,31 @@ class Agent:
 		Returns:
 			The action the agent is taking.
 		"""
-		return None
+		# perform update
+		S = self.lastState
+		A = self.lastAction
+		Sp = state
+		alpha = self.stepSize
+		R = reward
+		self.Q[(S,A)] += alpha*(R + max([self.Q[(Sp, a)] for a in self.actions]) - self.Q[(S,A)])
+		
+		# update observed states
+		if S not in self.observedStates:
+			self.observedStates[S] = []
+		if A not in self.observedStates[S]:
+			self.observedStates[S].append(A)
+		
+		# update model
+		self.model[(S,A)] = (R,Sp)
+
+		# plan
+		self.plan(reward)
+
+		# set up for next step
+		self.lastAction = self.epGreedy(state)
+		self.lastState = state
+		
+		return self.lastAction
 	
 	def agent_end(self, reward):
 		"""
@@ -54,7 +103,25 @@ class Agent:
 			reward (float): the reward the agent received for entering the
 				terminal state.
 		"""
-		pass
+		# perform update
+		S = self.lastState
+		A = self.lastAction
+		Sp = state
+		alpha = self.stepSize
+		R = reward
+		self.Q[(S,A)] += alpha*(R + max([self.Q[(Sp, a)] for a in self.actions]) - self.Q[(S,A)])
+		
+		# update observed states
+		if S not in self.observedStates:
+			self.observedStates[S] = []
+		if A not in self.observedStates[S]:
+			self.observedStates[S].append(A)
+		
+		# update model
+		self.model[(S,A)] = (R,Sp)
+
+		# plan
+		self.plan(reward)
 
 	def agent_message(self, message):
 		"""
@@ -64,4 +131,19 @@ class Agent:
 		returns:
 			str : the agent's response to the message (optional)
 		"""
+		pass
 
+	def epGreedy(self, state):
+		if np.random.rand() < self.epsilon: # prob ep
+			# choose random action
+			return np.random.choice(self.actions)
+		else: # prob 1-ep
+			# choose best action
+			return max(self.actions, key=lambda x: self.Q[(state,x)])
+
+	def plan(self, reward):
+		for _ in range(self.planningSteps):
+			S = self.lastState
+			A = np.random.choice(self.observedStates[S])
+			R, Sp = self.model[(S,A)]
+			self.Q[(S,A)] += self.stepSize*(reward + max([self.Q[(Sp, a)] for a in self.actions]) - self.Q[(S,A)])
